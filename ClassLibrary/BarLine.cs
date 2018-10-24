f0d150306531cae6c2cf9f0a5101a1935aab89a2
Sagar Gangabisoensingh <sagartje1993@gmail.com>
2018-10-24 12:49:51 +0200

    Barline list Notes

--- ClassLibrary/BarLine.cs
+++ ClassLibrary/BarLine.cs
@@ -13,7 +13,7 @@ namespace ClassLibrary
         public BarLine Buddy { get; set; }
         private bool HasBeenPlayed;
         public TYPE Type { get; set; }
-        private List<Alternative> Alternatives;
+        private List<Note> Alternatives;
 
         public override void GetSymbol()
         {
