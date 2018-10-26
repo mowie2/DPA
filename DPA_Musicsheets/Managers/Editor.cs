2a05d76395f4742971e51f745f3b92b7c724adf8
Mohamed <mhamoudi@avans.nl>
2018-10-26 11:37:03 +0200

    asdasdasd

--- DPA_Musicsheets/Managers/Editor.cs
+++ DPA_Musicsheets/Managers/Editor.cs
@@ -1,4 +1,6 @@
-﻿using DPA_Musicsheets.Facade;
+﻿using ClassLibrary;
+using DPA_Musicsheets.Converters;
+using DPA_Musicsheets.Facade;
 using DPA_Musicsheets.ViewModels;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
@@ -11,5 +13,14 @@ namespace DPA_Musicsheets.Managers
 {
     public class Editor : ViewModelBase
     {
+        private DomainToLily converter;
+        public Editor()
+        {
+            converter = new DomainToLily();
+        }
+        public string TextChanged()
+        {
+            return converter.GetLilyText(new Note());
+        }
     }
 }
--- DPA_Musicsheets/ViewModels/LilypondViewModel.cs
+++ DPA_Musicsheets/ViewModels/LilypondViewModel.cs
@@ -2,6 +2,8 @@
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.CommandWpf;
 using System;
+using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace DPA_Musicsheets.ViewModels
@@ -11,7 +13,7 @@ namespace DPA_Musicsheets.ViewModels
         private MusicLoader _musicLoader;
         private MainViewModel _mainViewModel { get; set; }
         private MusicController musicController;
-
+        private Editor editor;
         private string _text;
         private string _previousText;
         private string _nextText;
@@ -38,11 +40,11 @@ namespace DPA_Musicsheets.ViewModels
         }
 
         private bool _textChangedByLoad = false;
-        private readonly DateTime _lastChange;
+        private  DateTime _lastChange;
         private static readonly int MILLISECONDS_BEFORE_CHANGE_HANDLED = 1500;
-        private readonly bool _waitingForRender = false;
+        private  bool _waitingForRender = false;
 
-        public LilypondViewModel(MainViewModel mainViewModel, MusicLoader musicLoader, MusicController msc)
+        public LilypondViewModel(MainViewModel mainViewModel, MusicLoader musicLoader, MusicController msc, Editor edit)
         {
 
             // TODO: Can we use some sort of eventing system so the managers layer doesn't have to know the viewmodel layer and viewmodels don't know each other?
@@ -50,7 +52,7 @@ namespace DPA_Musicsheets.ViewModels
             _mainViewModel = mainViewModel;
             _musicLoader = musicLoader;
             _musicLoader.LilypondViewModel = this;
-
+            editor = edit;
             _text = "Your lilypond text will appear here.";
             musicController = msc;
         }
@@ -65,30 +67,31 @@ namespace DPA_Musicsheets.ViewModels
         /// <summary>
         /// This occurs when the text in the textbox has changed. This can either be by loading or typing.
         /// </summary>
-        //public ICommand TextChangedCommand => new RelayCommand<TextChangedEventArgs>((args) =>
-        //{
-        //    // If we were typing, we need to do things.
-        //    if (!_textChangedByLoad)
-        //    {
-        //        _waitingForRender = true;
-        //        _lastChange = DateTime.Now;
+        public ICommand TextChangedCommand => new RelayCommand<TextChangedEventArgs>((args) =>
+        {
+            editor.TextChanged();
+            // If we were typing, we need to do things.
+            if (!_textChangedByLoad)
+            {
+                _waitingForRender = true;
+                _lastChange = DateTime.Now;
 
-        //        _mainViewModel.CurrentState = "Rendering...";
+                _mainViewModel.CurrentState = "Rendering...";
 
-        //        Task.Delay(MILLISECONDS_BEFORE_CHANGE_HANDLED).ContinueWith((task) =>
-        //        {
-        //            if ((DateTime.Now - _lastChange).TotalMilliseconds >= MILLISECONDS_BEFORE_CHANGE_HANDLED)
-        //            {
-        //                _waitingForRender = false;
-        //                UndoCommand.RaiseCanExecuteChanged();
+                Task.Delay(MILLISECONDS_BEFORE_CHANGE_HANDLED).ContinueWith((task) =>
+                {
+                    if ((DateTime.Now - _lastChange).TotalMilliseconds >= MILLISECONDS_BEFORE_CHANGE_HANDLED)
+                    {
+                        _waitingForRender = false;
+                        UndoCommand.RaiseCanExecuteChanged();
 
-        //                //_musicLoader.LoadLilypondIntoWpfStaffsAndMidi(LilypondText);
-        //                musicController.SetStaffs();
-        //                _mainViewModel.CurrentState = "";
-        //            }
-        //        }, TaskScheduler.FromCurrentSynchronizationContext()); // Request from main thread.
-        //    }
-        //});
+                        //_musicLoader.LoadLilypondIntoWpfStaffsAndMidi(LilypondText);
+                        musicController.SetStaffs();
+                        _mainViewModel.CurrentState = "";
+                    }
+                }, TaskScheduler.FromCurrentSynchronizationContext()); // Request from main thread.
+            }
+        });
 
         #region Commands for buttons like Undo, Redo and SaveAs
         public RelayCommand UndoCommand => new RelayCommand(() =>
